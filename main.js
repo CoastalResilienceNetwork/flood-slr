a787c9207b0662ccf5a3f679879945a3732f6d3d
barry_focalpointgeo <barry@focalpointgeo.com>
2019-07-26 12:43:16 -0700

    update for single plugin mode help
    update for single plugin mode help

--- main.js
+++ main.js
@@ -56,6 +56,11 @@ define([
 
                activate: function () {
 					//console.log("activate");
+					if (this._firstLoad && this.app.singlePluginMode) {
+						$('#show-single-plugin-mode-help').click();
+						$('body').removeClass('pushy-open-left').removeClass('pushy-open-right');
+					}
+					
 					this.slr._status = "active";
 					if (_.isUndefined(this.map.getLayer("slr-layer-0"))) {
 						var plugin = this;
